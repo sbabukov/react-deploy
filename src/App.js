6cdf9eda9a7ce78d38401095e358eadb18785d8b
sbabukov <s.babukov@gmail.com>
2021-06-20 22:09:20 +0300

    add paragraph

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
      <h1>My React App</h1>
      <h2>Author: Stefan Babukov</h2>
+     <p>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nobis, vitae reprehenderit dolorum dolor unde provident officiis exercitationem eum cupiditate ipsa autem molestiae ab vero dicta. Quae, nihil? Qui, aperiam?
+     </p>
      <img src={profileimage} alt="profile-image" />
     </div>
   );
